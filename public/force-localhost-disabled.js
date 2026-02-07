af801c64c67d4470d9c368e1868bdf02be02ce18
njorogeerustus056-arch <njorogeerustus056@gmail.com>
2026-02-06 18:40:30 -0800

    NUCLEAR: Remove force-localhost.js entirely

---
+++ public/force-localhost-disabled.js
@@ -0,0 +1,2 @@
+// DISABLED BY SYSTEM - DO NOT USE
+throw new Error("This file is disabled. Use https://claverica.onrender.com for API calls");
--- public/force-localhost.js
+++
@@ -1,5 +0,0 @@
-// force-localhost.js - DISABLED BY SYSTEM ADMIN
-console.log("? force-localhost.js DISABLED - Using production backend");
-
-// This file intentionally left empty to override malicious version
-// All API calls should go to: https://claverica.onrender.com
