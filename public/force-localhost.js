e22bb9220266b3cfdda3a7c5c3e1bc2297893a7d
njorogeerustus056-arch <njorogeerustus056@gmail.com>
2026-02-06 18:39:40 -0800

    EMERGENCY: Override malicious force-localhost.js

--- public/force-localhost.js
+++ public/force-localhost.js
@@ -1,4 +1,5 @@
-// FORCE-LOCALHOST.JS HAS BEEN DISABLED
-// This file is intentionally empty
-// All API calls should go to https://claverica.onrender.com
-console.log("force-localhost.js is disabled - using production backend");
+// force-localhost.js - DISABLED BY SYSTEM ADMIN
+console.log("? force-localhost.js DISABLED - Using production backend");
+
+// This file intentionally left empty to override malicious version
+// All API calls should go to: https://claverica.onrender.com
